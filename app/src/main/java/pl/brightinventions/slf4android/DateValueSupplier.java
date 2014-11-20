d344212f44667b8699f13b1acda2cf880ca543bd
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2014-11-20 21:27:33 +0100

    Fix lint warning about locale

--- app/src/main/java/pl/brightinventions/slf4android/DateValueSupplier.java
+++ app/src/main/java/pl/brightinventions/slf4android/DateValueSupplier.java
@@ -1,12 +1,13 @@
 package pl.brightinventions.slf4android;
 
 import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 class DateValueSupplier implements LoggerPatternValueSupplier {
     private static ThreadLocal<SimpleDateFormat> dateFormat = new ThreadLocal<SimpleDateFormat>() {
         @Override
         protected SimpleDateFormat initialValue() {
-            return new SimpleDateFormat("yyyy.MM.dd HH:mm:ss.SSS");
+            return new SimpleDateFormat("yyyy.MM.dd HH:mm:ss.SSS", Locale.US);
         }
     };
 
