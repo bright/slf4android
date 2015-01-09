397f09c0ba6e416fa7e2b76dee052e6cc3721e4f
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2015-01-09 23:35:12 +0100

    Expose additional file handler configuration

--- app/src/main/java/pl/brightinventions/slf4android/FileLogHandlerConfiguration.java
+++ app/src/main/java/pl/brightinventions/slf4android/FileLogHandlerConfiguration.java
@@ -3,6 +3,13 @@ package pl.brightinventions.slf4android;
 import java.util.logging.Handler;
 
 public abstract class FileLogHandlerConfiguration extends Handler {
+    @SuppressWarnings("UnusedDeclaration")
     public abstract FileLogHandlerConfiguration setFullFilePathPattern(String fullPathPattern);
+
+    @SuppressWarnings("UnusedDeclaration")
+    public abstract FileLogHandlerConfiguration setRotateFilesCountLimit(int count);
+
+    @SuppressWarnings("UnusedDeclaration")
+    public abstract FileLogHandlerConfiguration setLogFileSizeLimitInBytes(int maxFileSizeInBytes);
     public abstract String getCurrentFileName();
 }
