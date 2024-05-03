b9b7a59b87d8ada7d5ebc29c74c67855df8f8831
Andrzej Zabost <5508894+azabost@users.noreply.github.com>
2024-05-03 13:29:23 +0200

    Fix incorrect usage of ActivityTestRule and incorrect imports in tests

--- handler-notify-developer/src/androidTest/java/pl/brightinventions/slf4android/handler/notifydeveloper/NotifyDeveloperHandlerTests.java
+++ handler-notify-developer/src/androidTest/java/pl/brightinventions/slf4android/handler/notifydeveloper/NotifyDeveloperHandlerTests.java
@@ -7,6 +7,7 @@ import android.app.Application;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,14 +16,14 @@ import java.util.concurrent.TimeUnit;
 
 import pl.brightinventions.slf4android.LoggerConfiguration;
 
-public class NotifyDeveloperHandlerTests extends ActivityTestRule<TestActivity> {
+public class NotifyDeveloperHandlerTests {
+
+    @Rule
+    public ActivityTestRule<TestActivity> activityTestRule =
+            new ActivityTestRule<>(TestActivity.class, true, true);
 
     private Logger LOG;
     private NotifyDeveloperHandler handler;
-
-    public NotifyDeveloperHandlerTests() {
-        super(TestActivity.class);
-    }
 
     @Before
     public void setUp() {
@@ -36,13 +37,13 @@ public class NotifyDeveloperHandlerTests extends ActivityTestRule<TestActivity> 
 
     @Test
     public void test_dont_send_message_with_level_lower_than_error() {
-        getActivity();
+        activityTestRule.getActivity();
         LOG.warn("Hello");
     }
 
     @Test
     public void test_send_message_with_level_error() throws Exception {
-        getActivity();
+        activityTestRule.getActivity();
         LOG.warn("Hello");
         LOG.error("Send email", new NullPointerException("A test message"));
         Thread.sleep(TimeUnit.SECONDS.toMillis(15));
@@ -51,7 +52,7 @@ public class NotifyDeveloperHandlerTests extends ActivityTestRule<TestActivity> 
     @Test
     public void test_send_message_with_custom_subject_body_with_level_error() throws Exception {
         handler.withSubject("Błąd").withBody("Podaj szczegóły błędu: ");
-        getActivity();
+        activityTestRule.getActivity();
         LOG.warn("Hello");
         LOG.error("Send email", new NullPointerException("A test message"));
         Thread.sleep(TimeUnit.SECONDS.toMillis(15));
